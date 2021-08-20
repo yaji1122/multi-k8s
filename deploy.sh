969eecf5fcea0f8b1b8e64b70b64bb38dfdeb4cc
yaji <yajilin@fontrip.com>
2021-08-20 11:52:06 +0800

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t yajilin/multi-client:latest -t yajilin/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t yajilin/multi-server:latest -t yajilin/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t yajilin/multi-worker:latest -t yajilin/multi-server:$SHA -f ./worker/Dockerfile ./worker
+
+docker push yajilin/multi-client:latest
+docker push yajilin/multi-server:latest
+docker push yajilin/multi-worker:latest
+
+docker push yajilin/multi-client:$SHA
+docker push yajilin/multi-server:$SHA
+docker push yajilin/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployment/server-deployment server=yajilin/multi-server:$SHA
+kubectl set image deployment/client-deployment client=yajilin/multi-client:$SHA
+kubectl set image deployment/worker-deployment client=yajilin/multi-worker:$SHA
